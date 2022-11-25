000df3f1c47dc874f95632e1e9db34d4a32c96ec
Yordi <yoriyordina7@gmail.com>
2022-11-25 13:10:38 +0300

    update

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ function itClicked(){
     exit.classList.add('for-exit');
     nav.appendChild(exit);    
 }
+/* for scrolling underlines in desktop */
+const sections = document.querySelectorAll(".first-page, .about, footer");
+const navs = document.querySelectorAll(".detail-nav ul li");
+
+console.log(navs);
+console.log(sections);
+
+window.addEventListener('scroll', scrollTO);
+
+function scrollTO(){
+    var current;
+    sections.forEach(section=>{
+    let sectionTop = section.offsetTop;
+    let sectionHeight = section.clientHeight;
+    console.log(pageYOffset)
+    if(pageYOffset >= ((sectionTop - sectionHeight) / 3)){
+    current = section.getAttribute('id');
+    }
+    })
+    for(let i='0'; i<navs.length; i++){
+        if(current===i){
+            console.log(current===i, current, i)
+        li.style.textDecoration = "underline overline";
+       }
+    }
+    navs.forEach(li=>{
+       
+    })
+}
+
+// end of scroll here  */
+// alert(section1+" "+ section2 +" "+section3);  */
+
