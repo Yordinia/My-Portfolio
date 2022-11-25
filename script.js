226bfbb09dc68c292fb5791241aacaedee7bcbc7
Yordi <yoriyordina7@gmail.com>
2022-11-26 00:33:35 +0300

    justify content

--- script.js
+++ script.js
@@ -50,18 +50,21 @@ const evThing = document.querySelectorAll('nav, main, footer');
 const dispProject = document.querySelectorAll('.disp-project');
 const section = document.createElement('section');
 section.innerHTML = `
+  <a href='#portfolio' class='for-desktop-nav'></a>
   <div class="card-image"><a href='#portfolio'></a></div>
-  <h4>Keeping track of hundreds of components</h4>
-  <ul class="languages">
+  <div class='try'>
+  <h4 id='id1'>Keeping track of hundreds of components</h4>
+  <ul class="languages" id='id2'>
   <li><a href="#">Ruby on rails</a></li>
   <li><a href="#">css</a></li>
   <li><a href="#">JavaScript</a></li>
   </ul>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.
+  <p id='id3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
-  <div class='to-flex'>
+  <div class='to-flex' id='id4'>
   <div class="disp-project">See Live <img src='./images/seeLive.svg' width='18px' height= '18px'></div>
   <div class="disp-project">See Source <img src='./images/seeSource.svg' width= '18px'  height= '18px'></div>
+  </div>
   </div>
   `;
 section.classList.add('popup-style');
