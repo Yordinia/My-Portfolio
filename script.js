78d7ac298c30f268f40e7d25109fd102a6461552
Yordi <yoriyordina7@gmail.com>
2022-11-25 21:29:53 +0300

    Add popup style

--- script.js
+++ script.js
@@ -43,3 +43,37 @@ function itClicked() {
 }
 
 menu.addEventListener('click', itClicked);
+
+/*       ````` POP UP `````  */
+
+const evThing = document.querySelectorAll('nav, main, footer');
+const dispProject = document.querySelectorAll('.disp-project');
+const section = document.createElement('section');
+section.innerHTML = `
+  <div class="card-image"></div>
+  <h4>Keeping track of hundreds of components</h4>
+  <ul class="languages">
+  <li><a href="#">Ruby on rails</a></li>
+  <li><a href="#">css</a></li>
+  <li><a href="#">JavaScript</a></li>
+  </ul>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+  <div class='to-flex'>
+  <div class="disp-project">See Live <img src='./images/seeLive.svg' width='18px' height= '18px'></div>
+  <div class="disp-project">See Source <img src='./images/seeSource.svg' width= '18px'  height= '18px'></div>
+  </div>
+  `;
+
+section.classList.add('popup-style');
+
+function popup(){
+  evThing.forEach(element=> element.style.display='none')
+  document.body.classList.add('for-body');
+  document.body.appendChild(section);
+}
+
+dispProject.forEach(buttons=> buttons.addEventListener('click', popup));
+
+
+/*``````````*/
